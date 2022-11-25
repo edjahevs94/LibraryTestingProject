c0dc7b3cd470c2d48daf74b6f7333ff6da34dd17
Edgard <20133252@ealoe.ulima.edu.pe>
2022-11-25 09:44:15 -0500

    Initial Commit

---
+++ ExampleLibraryProject/ExampleLibraryProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleLibraryProjectApp.swift
+//  ExampleLibraryProject
+//
+//  Created by EdgardVS on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleLibraryProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
